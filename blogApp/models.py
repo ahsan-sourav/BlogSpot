f9c4b2f4098d118d06a961892681f90f4d05b17f
Tahsinul Muntasir Moon <muntasirmarshmallow@gmail.com>
2025-02-28 21:00:18 +0600

    Tahsinul Muntasir Moonn

--- blogApp/models.py
+++ blogApp/models.py
@@ -6,3 +6,15 @@ class Contact(models.Model):
     email = models.EmailField(max_length=600)
     subject = models.CharField(max_length=1000)
     message = models.CharField(max_length=10000, blank=True)
+
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    time = models.CharField(default=time, max_length=100, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}.{self.content[:20]}..."
+
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html", {
+        'posts': Post.objects.filter(user_id=request.user.id).order_by("id").reverse(),
+        'top_posts': Post.objects.all().order_by("-likes"),
+        'recent_posts': Post.objects.all().order_by("-id"),
+        'user': request.user,
+        'media_url': settings.MEDIA_URL
+    })
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username already Exists")
+                return redirect('signup')
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already Exists")
+                return redirect('signup')
+            else:
+                User.objects.create_user(username=username, email=email, password=password).save()
+                return redirect('signin')
+        else:
+            messages.info(request, "Password should match")
+            return redirect('signup')
+
+    return render(request, "signup.html")
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return redirect("signin")
+
+    return render(request, "signin.html")
